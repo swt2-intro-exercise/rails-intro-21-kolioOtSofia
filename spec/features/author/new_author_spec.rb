8f841bc8b538dc546a01f6b2bff2ad8ec7a16226
Nikola Genchev <n.genchev@hotmail.com>
2021-11-10 09:35:45 +0100

    first test

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "New author page", type: :feature do
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-    visit new_authors_path
+    visit new_author_path
   end
 end
