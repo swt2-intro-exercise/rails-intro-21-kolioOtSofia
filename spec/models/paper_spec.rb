75b9c9fe8e239d3df9b8153a7f279970cba7e91a
Nikola Genchev <n.genchev@hotmail.com>
2021-11-15 18:37:56 +0100

    unworking test issue 14

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "tests validation" do
+    paper = Paper.new
+    expect(paper).to_not be_valid
+    paper.title = "idk dude"
+    expect(paper).to_not be_valid
+    paper.venue = "idk dude"
+    expect(paper).to_not be_valid
+    paper.year = 2004
+    expect(paper).to be_valid
+  end
 end
